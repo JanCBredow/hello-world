94221739e111047ecd9adc6946739bef070d4459
Jan Christopher Bredow <jan.bredow@epeu.dev>
2022-01-04 16:28:03 +0100

    add hello-world in Java

---
+++ hello-world.java
@@ -0,0 +1,7 @@
+public final class HelloWorld {
+  private static final String HELLO_WORLD_TEXT = "Hello, World!";
+
+  public static void main(String[] options){
+    System.out.print(HELLO_WORLD_TEXT + "\n");
+  }
+}
